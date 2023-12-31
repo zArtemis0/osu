267b45fa8b74b18238d62bad4a790bb687247363
OliBomby <olivierschipper@gmail.com>
2023-12-31 16:41:22 +0100

    add integer keyboard step to sliders

--- osu.Game.Rulesets.Osu/Edit/OsuGridToolboxGroup.cs
+++ osu.Game.Rulesets.Osu/Edit/OsuGridToolboxGroup.cs
@@ -126,18 +126,22 @@ namespace osu.Game.Rulesets.Osu.Edit
                 startPositionXSlider = new ExpandableSlider<float>
                 {
                     Current = StartPositionX,
+                    KeyboardStep = 1,
                 },
                 startPositionYSlider = new ExpandableSlider<float>
                 {
-                    Current = StartPositionY
+                    Current = StartPositionY,
+                    KeyboardStep = 1,
                 },
                 spacingSlider = new ExpandableSlider<float>
                 {
-                    Current = Spacing
+                    Current = Spacing,
+                    KeyboardStep = 1,
                 },
                 gridLinesRotationSlider = new ExpandableSlider<float>
                 {
-                    Current = GridLinesRotation
+                    Current = GridLinesRotation,
+                    KeyboardStep = 1,
                 },
                 gridFromPointsButton = new RoundedButton
                 {
